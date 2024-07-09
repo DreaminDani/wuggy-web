e347a1efa029f333a959ab91ac4348b4014e4906
Dani Sandoval <dani@equall.ai>
2024-07-08 20:58:24 -0600

    add wuggy to requirements and create a command line only poc

---
+++ poc/README.md
@@ -0,0 +1,14 @@
+## Installing dependencies
+
+Run `pip install -r requirements.txt`.
+
+## Getting new words
+To generate pseudowords using the Wuggy server, you can use the following methods:
+
+### Using the CLI
+Run the following command to generate pseudowords via the command line interface:
+
+Run `python main.py --referenceSequence trumpet`
+To change the number of generated words use `--wordLength` like `python main.py --referenceSequence trumpet --wordLength 5`
+
+### Using the websever (coming soon)
---
+++ poc/main.py
@@ -0,0 +1,55 @@
+import os
+import time
+import argparse
+from typing import Dict, Optional
+
+from wuggy import WuggyGenerator
+
+environment = os.environ.get("ENV", "development")
+if environment == "production":
+    print("Wuggy Server is set to production mode. All supported language plugins will be loaded.")
+    language_plugins_to_load = WuggyGenerator.supported_language_plugins.keys()
+else:
+    print("Wuggy Server is set to development mode. Only a subset of language plugins will be loaded.")
+    language_plugins_to_load = ["orthographic_english"]
+    # Uncomment if your language plugin is missing
+    # wuggy_downloader = WuggyGenerator()
+    # wuggy_downloader.download_language_plugin("orthographic_english", True)
+
+wuggy_generators: Dict[str, WuggyGenerator] = {}
+for language_plugin_name in language_plugins_to_load:
+    print(f"Loading language plugin {language_plugin_name}")
+    generator = WuggyGenerator()
+    generator.load(language_plugin_name)
+    wuggy_generators[language_plugin_name] = generator
+
+print(
+    f"Starting Wuggy server with the following loaded language plugins: {wuggy_generators.keys()}")
+
+def generate_simple_cli(reference_sequence: str, language_plugin: str = "orthographic_english", word_length: int = 10):
+    """
+    Example usage: python main.py --referenceSequence trumpet --wordLength 5
+    """
+    if language_plugin in wuggy_generators:
+        wuggy_generator = wuggy_generators[language_plugin]
+        pseudowords = []
+        # for sequence in wuggy_generator.generate_classic([reference_sequence]):
+        for sequence in wuggy_generator.generate_classic([reference_sequence],
+    ncandidates_per_sequence=word_length, max_search_time_per_sequence=25):
+            pseudowords.append(sequence["pseudoword"])
+        return { "word": reference_sequence, "matches": pseudowords }
+    else:
+        print(f"Language plugin {language_plugin} not found.")
+        return None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate pseudowords using Wuggy.")
+    parser.add_argument("--referenceSequence", type=str, required=True, help="The reference sequence to generate pseudowords for.")
+    parser.add_argument("--languagePlugin", type=str, default="orthographic_english", help="The language plugin to use.")
+    parser.add_argument("--wordLength", type=int, default=10, help="The desired number of pseudowords to generate.")
+    
+    args = parser.parse_args()
+    
+    result = generate_simple_cli(args.referenceSequence, args.languagePlugin, args.wordLength)
+    if result:
+        print(result)
